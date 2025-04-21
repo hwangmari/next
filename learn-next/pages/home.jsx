7667ac6b364f2d7797ecc10db8273d16a1ae995b
hwangmari <hwangmari@naver.com>
2025-04-21 23:01:28 +0900

    상품목록 페이지

---
+++ learn-next/pages/home.jsx
@@ -0,0 +1,4 @@
+function HomePage( ) {
+  return <div>hello next</div>
+}
+export default HomePage;
---
+++ learn-next/pages/login.jsx
@@ -0,0 +1,4 @@
+function LoginPage () {
+    return <div>로그인페이지</div>
+}
+export default LoginPage;
